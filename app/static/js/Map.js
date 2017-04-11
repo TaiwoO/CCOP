1104856beae6b77fcd4624e3a68365a8216a502d
Robert <arejaybee@gmail.com>
2017-04-11 18:30:19 -0400

    reset script for now

--- app/static/js/Map.js
+++ app/static/js/Map.js
@@ -2,7 +2,8 @@
 //implements the map view
 
 // Google map automatically calls this function
-function initMap() {
+export function initMap()
+{
     var uluru = { lat: -25.363, lng: 131.044 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
