54e401d82736d01915cdfe6dea328481ecc547bc
Robert <arejaybee@gmail.com>
2017-04-13 21:13:29 -0400

    trying to read data...

--- app/static/js/Map.js
+++ app/static/js/Map.js
@@ -1,22 +1,38 @@
 //Writen by Robert Bradshaw
 //implements the map view
 
+function getData(callback)
+{
+    $.getJSON("/crime");
+}
 
 //googlemaps automatically calls this
 function initMap()
 {
     var minZoomLevel = 10;
     var uluru = { lat: 39.154743, lng: -77.240515 };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'),{
         zoom: minZoomLevel,
         center: uluru
     });
- 
     
-    new google.maps.Marker({
-        position: uluru,
-        map: map
+    getData(function(data){
+	var crimes = data.crimes;
+	var crime, latLng;
+	for(i in crimes){
+	    console.log(i);
+	    crime = crimes[i];
+	    latLng = new google.maps.LatLng(crime.latitude, crime.longitude);
+	    
+	    var marker = new google.maps.Marker({
+		position:latLng,
+		map: map
+	    });
+	}
     });
+    
+    limitMap(map,minZoomLevel);
+    
 }
 
 //limits the map
