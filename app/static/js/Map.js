1b36e87e71a7f85680bf61f5ee4916cfdfbc2b65
Robert <arejaybee@gmail.com>
2017-04-15 03:28:30 -0400

    Map now has label for Crime points, and it will give more info when hovering over marker

--- app/static/js/Map.js
+++ app/static/js/Map.js
@@ -1,11 +1,6 @@
 //Writen by Robert Bradshaw
 //implements the map view
 
-function getData(callback)
-{
-    $.getJSON("/crime");
-}
-
 //googlemaps automatically calls this
 function initMap()
 {
@@ -15,23 +10,29 @@ function initMap()
         zoom: minZoomLevel,
         center: uluru
     });
-    
-    getData(function(data){
-	var crimes = data.crimes;
-	var crime, latLng;
-	for(i in crimes){
-	    console.log(i);
-	    crime = crimes[i];
-	    latLng = new google.maps.LatLng(crime.latitude, crime.longitude);
-	    
-	    var marker = new google.maps.Marker({
-		position:latLng,
-		map: map
-	    });
-	}
-    });
-    
-    limitMap(map,minZoomLevel);
+
+    var crimeIcon = 'https://maps.google.com/mapfiles/kml/shapes/purple_stars_maps.png';
+     $.getJSON($SCRIPT_ROOT + "/crime",{}, function (data) {
+        console.log(data);
+         //work with the data here
+	 var crimes = data.crimes;
+	 var crime, latLng;
+	 for(i in crimes){
+	     console.log(i);
+	     crime = crimes[i];
+	     latLng = new google.maps.LatLng(crime.latitude, crime.longitude);
+	     
+	     var marker = new google.maps.Marker({
+		 position:latLng,
+		 map: map,
+		 title: "Crime\n"+crime.description+"\n"+crime.dispatch+"\n"+crime.street,
+		 label: "C"
+//		 icon: crimeIcon
+	     });
+	 }
+     });
+        
+    //limitMap(map,minZoomLevel);
     
 }
 
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -19,4 +19,5 @@ function initApp() {
 
 initApp()
 getJson("/crime")
+
 console.log("SCRIPT WORKING!!")
