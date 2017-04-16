b8dea0229205fe143a3b490492945c8ab503c566
Robert <arejaybee@gmail.com>
2017-04-15 20:13:44 -0400

    Map gets markers from function now

--- app/static/js/Map.js
+++ app/static/js/Map.js
@@ -11,29 +11,33 @@ function initMap()
         center: uluru
     });
 
-    var crimeIcon = 'https://maps.google.com/mapfiles/kml/shapes/purple_stars_maps.png';
-     $.getJSON($SCRIPT_ROOT + "/crime",{}, function (data) {
-        console.log(data);
-         //work with the data here
-	 var crimes = data.crimes;
-	 var crime, latLng;
-	 for(i in crimes){
-	     console.log(i);
-	     crime = crimes[i];
-	     latLng = new google.maps.LatLng(crime.latitude, crime.longitude);
-	     
-	     var marker = new google.maps.Marker({
-		 position:latLng,
-		 map: map,
-		 title: "Crime\n"+crime.description+"\n"+crime.dispatch+"\n"+crime.street,
-		 label: "C"
-//		 icon: crimeIcon
-	     });
-	 }
-     });
-        
+    updateMarkers(map);
+              
     //limitMap(map,minZoomLevel);
     
+}
+
+//Gets markers onto map
+function updateMarkers(map)
+{
+    $.getJSON($SCRIPT_ROOT + "/crime",{}, function (data) {
+        console.log(data);
+        //work with the data here
+	var crimes = data.crimes;
+	var crime, latLng;
+	for(i in crimes){
+	    console.log(i);
+	    crime = crimes[i];
+	    latLng = new google.maps.LatLng(crime.latitude, crime.longitude);
+	    
+	    var marker = new google.maps.Marker({
+		position:latLng,
+		map: map,
+		title: "Crime\n"+crime.description+"\n"+crime.dispatch+"\n"+crime.street,
+		label: "C"
+	    });
+	}
+    });
 }
 
 //limits the map
