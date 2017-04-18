934682c960c0cc7ee61bf95472423b0d558b2b25
JodyLeSage <jody.c.lesage@gmail.com>
2017-04-18 15:01:48 -0400

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -5,7 +5,8 @@ class RequestLimitReachedError(RuntimeError):
     def __init__(self):
         self.message = "Daily API query limit has been reached."
 
-# raised when only partial data has been added to the database
+# raised when the user runs out of api calls and
+# there are records that need to be geocoded
 class PartialDataError(RuntimeError):
     def __init__(self, records, typeOfRecord):
         self.message = "{0} {1}s have been added to the database".format(records, typeOfRecord)
