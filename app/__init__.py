b98d423b9f4cf78b401850fd3dadf83cb53b0e71
JodyLeSage <jody.c.lesage@gmail.com>
2017-04-18 15:08:30 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
+# this file describes behavior when the CCOP application is run
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# instantiate and configure the Flask application
 app = Flask(__name__)
 app.config.from_object('config')
+
+# set up object-relational mapping
 db = SQLAlchemy(app)
 
 # import at the end prevents circular dependencies
+# (views and models depend on app)
 from app import views, models
