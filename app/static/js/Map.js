99e71bda2cfafad0f74fa8bd4bf5b27c270ecd66
Robert <arejaybee@gmail.com>
2017-04-19 15:36:35 -0400

    Updated map to load arrest points

--- app/static/js/Map.js
+++ app/static/js/Map.js
@@ -21,9 +21,10 @@ function initMap()
 //Gets markers onto map
 function updateMarkers(map)
 {
-    $.getJSON($SCRIPT_ROOT + "/crime",{}, function (data) {
+
+    var url = "/crime?min_time=2017-04-01T15:47:13.657Z&max_time=2017-04-19T15:47:13.657Z&bounds=38.955865,-77.232668,39.1646,-77.055342";
+    $.getJSON($SCRIPT_ROOT+url,{}, function (data){
         console.log(data);
-        //work with the data here
 	var crimes = data.crimes;
 	var crime, latLng;
 	for(i in crimes){
@@ -36,6 +37,25 @@ function updateMarkers(map)
 		map: map,
 		title: "Crime\n"+crime.description+"\n"+crime.dispatch+"\n"+crime.street,
 		label: "C"
+	    });
+	}
+    });
+
+    url = "/arrest?min_time=2017-04-01T15:47:13.657Z&max_time=2017-04-19T15:47:13.657Z&bounds=38.955865,-77.232668,39.1646,-77.055342";
+    $.getJSON($SCRIPT_ROOT+url,{}, function (data){
+        console.log(data);
+	var arrests = data.arrests;
+	var arrest, latLng;
+	for(i in arrests){
+	    console.log(i);
+	    arrest = arrests[i];
+	    latLng = new google.maps.LatLng(arrest.latitude, arrest.longitude);
+	    
+	    var marker = new google.maps.Marker({
+		position:latLng,
+		map: map,
+		title: "Arrest\n"+arrest.date+"\n"+arrest.street,
+		label: "A"
 	    });
 	}
     });
