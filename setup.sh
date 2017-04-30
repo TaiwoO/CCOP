e9fb3b6f5bda658c357511d41d6edb03945e4b82
jody <jody.c.lesage@gmail.com>
2017-04-29 21:40:05 -0400

    removed unused dependencies

--- setup.sh
+++ setup.sh
@@ -17,20 +17,5 @@ python3 -m venv ccop_env
 ccop_env/bin/pip3 install wheel		# needed to build wheels
 ccop_env/bin/pip3 install flask
 ccop_env/bin/pip3 install flask-sqlalchemy
-ccop_env/bin/pip3 install sqlalchemy-migrate
+#ccop_env/bin/pip3 install sqlalchemy-migrate
 ccop_env/bin/pip3 install requests
-
-# have yet to find a reason to have apache
-# sudo apt-get install apache2
-
-# Decided on SQLite instead
-# install and setup mysql
-# sudo apt-get install mysql-server
-# sudo mysql_secure_installation
-# sudo mysqld --initialize
-
-# install python 2.7, pip, and the requests library
-# sudo apt-get install python2.7
-# sudo apt-get install python-pip
-# sudo -H pip install --upgrade pip
-# sudo -H pip install requests
