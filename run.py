fa3d94316e903a1572bb120a3641c1eb9e3c808f
jody <jody.c.lesage@gmail.com>
2017-04-29 21:49:32 -0400

    database creation rolled into setup.sh

--- run.py
+++ run.py
@@ -2,4 +2,5 @@
 
 # this file runs the CCOP web app from the virtual environment
 from app import app
+# TODO set debug to False before final delivery
 app.run(debug=True)
--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # this file is written to make the application easy to set up
 # when run as root, it installs dependencies and
 # sets up a python virtual environment in which to run
+# additionally, it builds an empty database
+
 
 # update apt
 sudo apt-get update
@@ -19,3 +21,8 @@ ccop_env/bin/pip3 install flask
 ccop_env/bin/pip3 install flask-sqlalchemy
 #ccop_env/bin/pip3 install sqlalchemy-migrate
 ccop_env/bin/pip3 install requests
+
+
+# build the database schema
+source ccop_env/bin/activate
+./db_create.py
