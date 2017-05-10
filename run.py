f25665cb3d188b27570baa6e13a9a273fa266087
JodyLeSage <jody.c.lesage@gmail.com>
2017-05-10 19:38:47 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!ccop_env/bin/python3
+# author: Jody LeSage
 
 # this file runs the CCOP web app from the virtual environment
 from app import app
 # TODO set debug to False before final delivery
-app.run(debug=True)
+app.run(debug=False)
